2c376fc502ebfc364a2148dfd311b7b00c64a997
Rick G <rick030503@gmail.com>
2022-05-14 12:09:42 +0200

    14052022

--- modules/pomodoro.js
+++ modules/pomodoro.js
@@ -1,5 +1,31 @@
 //new Date().format('d-m-Y h:i:s');
 
+
+function getValue() {
+    // Sélectionner l'élément input et récupérer sa valeur
+    var input = document.getElementById("in").value;
+    // Afficher la valeur
+    return input;
+}
+
+function Timer(){
+    var currentDate = new Date()
+    var day = currentDate.getDate();
+    var month = currentDate.getMonth()+1;
+    var year = currentDate.getFullYear();
+    var hour = currentDate.getHours();
+    var minute = currentDate.getMinutes();
+    var second = currentDate.getSeconds();
+    var input = getValue();
+    if(parseInt(minute)+parseInt(input)>60){
+        var resulthour = parseInt(hour) + parseInt(minute)/60;
+        var resultminute = parseInt(minute)%60;
+    }
+    var result = parseInt(minute)+parseInt(input);
+    alert("Today we're the "+day+"/"+month+"/"+year+ " at "+hour+":"+minute+":"+second);
+    alert("This timer will end at "+hour+":"+result+":"+second);
+}
+
 /*
 var CurrentDate = new Date().toLocaleDateString();
 var CurrentTime = new Date().toLocaleTimeString();
@@ -14,6 +40,8 @@ var minute = currentDate.getMinutes();
 var second = currentDate.getSeconds();
 //console.log("Today we're the ",CurrentDate," and it is ",CurrentTime);
 console.log(day,month,year,hour,minute,second);
+
+/*
 while(0==0){
     var currentDate = new Date()
     var day = currentDate.getDate();
@@ -24,3 +52,4 @@ while(0==0){
     var second = currentDate.getSeconds();
     console.log(day,month,year,hour,minute,second);
 }
+*/
